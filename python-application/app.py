c27df621cab7ca4677873d2ad1fae9ac4db169f8
Badr <57539984+Imb4dr@users.noreply.github.com>
2020-10-08 22:03:52 +0100

    Updates on app.py and req

--- python-application/app.py
+++ python-application/app.py
@@ -1,11 +1,19 @@
 from flask import Flask
-app = Flask(__name__)
+import epsagon
 
+epsagon.init(
+    token='f41b1713-e48b-4685-a88b-34db5b3b0c55',
+    app_name='pipeline-azure-aks',
+    metadata_only=False
+)
+
+app = Flask(__name__)
+epsagon.flask_wrapper(app)
 
 @app.route('/')
 def hello():
     return "Stay inside, stay safe and keep social distancing."
 
 if __name__ == '__main__':
- app.run(debug=True,host='0.0.0.0',port=5000)
-app.run(host='0.0.0.0') 
+    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0') 
